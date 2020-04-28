75602ada3f36474fdf405b5df93c27c1f26df931
jimbomoso <jimbomoso@gmail.com>
2020-04-28 17:49:35 -0400

    Adds components folder, container component, add button and binding

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,22 @@
 <script>
+  import Container from '../components/Container.svelte';
   let todos = [];
   function addTodo(){
-
+    todos = [...todos, ''];
   }
   function removeSelf(){
 
   }
 </script>
 
+<Container>
+  <h1> Todos: </h1>
+  {#each todos as todo, index}
+    <input bind:value={todos[index]}>
+  {/each}
 
+  <button on:click={addTodo}>Add</button>
+</Container>
 <style>
 	main {
 		text-align: center;
